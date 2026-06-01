377f07e92dc03b76c3a0c372c29a33925a0bec71
thomas <al3ph_0@proton.me>
2026-06-01 14:15:53 +0400

    Rustlings 30/94

--- rustlings/exercises/04_primitive_types/primitive_types4.rs
+++ rustlings/exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,13 @@
 fn main() {
     // You can optionally experiment here.
+    let a = [1, 2, 3, 4, 5];
+    let b = 2;
+    print(b);
+    print(b);
+}
+
+fn print(n: i32) {
+    println!("{}", n);
 }
 
 #[cfg(test)]
@@ -9,7 +17,7 @@ mod tests {
         let a = [1, 2, 3, 4, 5];
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
-        // let nice_slice = ???
+        let nice_slice = &a[1..4];
 
         assert_eq!([2, 3, 4], nice_slice);
     }
